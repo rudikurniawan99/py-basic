9d1522487200567db8bb4c3eaf4012a682ab81e4
rudikurniawan99 <rudikurniawan501@gmail.com>
2025-04-29 09:03:49 +0700

    topic: import package

--- src/main.py
+++ src/main.py
@@ -1,2 +1,19 @@
+# import package
+import utils.helpers as hlp
+import math
+
 if __name__ == "__main__":
-    print("hello world")
+    # python loops
+    short_names = ["rk", "af", "am", "sn", "sm"]
+    # for key, name in short_names:
+    #    print(f"the name is: {name}, and key: {key}")
+    # if True:
+    #    print("test this is nothing to be done")
+    # hlp.helper_intro()
+    # hlp.print_names()
+    # make it possible or nonetheless
+
+    # make it possible to use math package
+    res = math.sin(70)
+    print(f"result: {res}")
+    hlp.helper_intro()
---
+++ src/utils/helpers.py
@@ -0,0 +1,8 @@
+def helper_intro():
+    print("this is helper intro")
+
+
+def print_names():
+    print("tobe")
+    print("maguire")
+    print("thomas")
