aa970f70e517fb27ff6e48a1f64cfee3a911299e
rudikurniawan99 <rudikurniawan501@gmail.com>
2025-04-30 14:24:14 +0700

    init: exception

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 # import package
-import utils.helpers as hlp
-import math
+import uuid
+from datetime import date
 
 if __name__ == "__main__":
     # python loops
@@ -14,6 +14,48 @@ if __name__ == "__main__":
     # make it possible or nonetheless
 
     # make it possible to use math package
-    res = math.sin(70)
-    print(f"result: {res}")
-    hlp.helper_intro()
+    # res = math.sin(70)
+    # print(f"result: {res}")
+    # hlp.helper_intro()
+
+    # Started OOP concept
+    """
+    Python OOP
+    => learn OOP concept in python
+    """
+    # notes:
+    # 1. __new__ creating an object
+    # 2. __init__ initialize the object with attribute,
+    # 3. __init__ initalize the object after creation
+
+    # class Game:
+    #    def __init__(self, name, genre):
+    #        print("Inside __init__ => initializing the object")
+    #        self.name = name
+    #        self.genre = genre
+
+    # g = Game("PUBG", "fps")
+    # g.name = "mole"
+    # print(g.name, g.genre)
+
+    # class Account:
+    #    def __init__(self, name, birth) -> None:
+    #        self.id = uuid.uuid4()
+    #        self.name = name
+    #        self.birht = birth
+
+    #    def print_data(self):
+    #        print("=====================")
+    #        print(f"ID : {self.id}")
+    #        print(f"name: {self.name}")
+    #        print(f"date: {self.birht}")
+    #        print("=====================")
+
+    # rk_account = Account("Rudi Kurniawan", date(1999, 2, 14))
+
+    # rk_account.print_data()
+
+    """
+    Python Exception
+    => learn deal with 'error' using  raise, and exception
+    """
